844bcf55688ece953b16fc8a6b70cb6528440fe1
Shamota1998 <51853990+Shamota1998@users.noreply.github.com>
2019-06-18 14:32:14 +0300

    Practice Java. Task One.
    Maven && JUnit Test. Version 1

---
+++ SumOfArray.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class SumOfArray {
+    private ArrayList <Integer> list = new ArrayList<Integer>();
+    private int summa =0;
+    SumOfArray() throws NullPointerException {
+        System.out.println("Input integer number to calculate the sum: (for exit press 0)");
+        int temp=-1;
+        try{
+        while(true){
+            System.out.print("Number to calculate :");
+            temp = new Scanner(System.in).nextInt();
+            if(temp==0){break;}
+            list.add(temp);
+            System.out.println("\n");
+        }
+        }catch(InputMismatchException e){
+            System.out.println("Please enter only integers!");
+        }
+    }
+
+    SumOfArray(int[] arr)throws NullPointerException{
+        for (int var:arr) {
+            list.add(var);
+        }
+    }
+
+    public int total(){
+        for (int arg:list) {
+            summa+=arg;
+        }
+        return summa;
+    }
+}
